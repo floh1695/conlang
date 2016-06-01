c23d0aca5bd8dcd74561b17da66ba52d2d062772
Charles Bayley <cbayley95@gmail.com>
2016-06-01 15:01:55 -0700

    Added the entrance file to the program.

---
+++ DictManager.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Dictonary Manager
+# -- This is the manager for my conlangs 
+# -- dictionary.
+# -- I may also bundle a grammer checker 
+# -- into this at some point.
+# -- If I'm feeling froggy I may also
+# -- give it thesaurus-like features.
+
+if __name__ == '__main__':
+    pass
+
